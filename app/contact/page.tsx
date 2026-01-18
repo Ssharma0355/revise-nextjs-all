ef68de8f01bdf114c7da2fc7080c0652960de04f
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-18 09:26:12 +0530

    Deffrence btween server and client component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const Contact = () => {
+    console.log("Is it run on clinet or server?")
+    const [count, setCount] = useState(0);
   return (
     <div>
       Contact
+      <h1>This is Client Component</h1>
+      <h1>See the state changes {count}</h1>
+      Wee can use buttons events and state only inside the client component not in server component
+      <br />
+      <button onClick={()=>setCount(count+1)}>Click me to add</button>
+      <br />
+      <button onClick={()=>setCount(count-1)}>Click me to substract</button>
+
     </div>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images:{
+    remotePatterns:[
+      {hostname:"upload.wikimedia.org"}
+      // we nee to add this in this file because it is a exteranl image and we supose to add this coz of optimization
+    ]
+  }
   /* config options here */
 };
 
